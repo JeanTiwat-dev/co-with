3e989649133ce7120b2084a7c5cd7398c65f03a7
JeanTiwat <82171998+JeanTiwat-dev@users.noreply.github.com>
2022-11-16 01:37:40 +0700

    Add backend

---
+++ backend/src/main/java/com/sop/backend/backend/pojo/user.java
@@ -0,0 +1,28 @@
+package com.sop.backend.backend.pojo;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+
+@Data
+@Document("User")
+public class user  implements Serializable {
+    @Id
+    private String _id;
+    private String email;
+    private String password;
+    private String img;
+    private String firstname;
+    private String lastname;
+    public user() {}
+    public user(String _id, String email, String password, String img, String firstname, String lastname) {
+        this._id = _id;
+        this.email = email;
+        this.password = password;
+        this.img = img;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+}
